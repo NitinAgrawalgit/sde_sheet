11191aff0413352b15b719ba47b8b05cb874b465
NitinAgrawalgit <83645245+NitinAgrawalgit@users.noreply.github.com>
2023-08-15 19:07:00 +0530

    Update setMatrixZero.cpp to Set Matrix Zeros.cpp

--- Set Matrix Zeros.cpp
+++ Set Matrix Zeros.cpp
@@ -1,5 +1,6 @@
 /** Problem Statement: 
 Given a matrix if an element in the matrix is 0 then you will have to set its entire column and row to 0 and then return the matrix.
+Link: https://leetcode.com/problems/set-matrix-zeroes/
 */
 
 //Optimal Code: 
